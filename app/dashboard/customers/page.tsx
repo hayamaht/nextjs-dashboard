08b26c49ada815787a534192e7957e75b5f35097
hayama <hayamaroom@gmail.com>
2024-02-01 12:01:03 +0800

    customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,24 @@
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function CustomersPage() {
+export default async function CustomersPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string
+  }
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
   return (
-    <div>CustomersPage</div>
+    <Table customers={customers} />
   )
 }
